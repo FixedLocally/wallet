fbefb7c7d23a7c2fa3a3300ab48bfef6a06ab2ed
FixedLocally <9797160+FixedLocally@users.noreply.github.com>
2022-08-08 21:31:44 +0800

    Sign messages

--- lib/routes/home.dart
+++ lib/routes/home.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:solana/base58.dart';
+import 'package:solana/encoder.dart';
 import '../rpc/key_manager.dart';
 import 'webview.dart';
 
@@ -15,6 +18,71 @@ class _HomeRouteState extends State<HomeRoute> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Wallet'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              TextEditingController controller = TextEditingController();
+              await showDialog<String>(
+                context: context,
+                builder: (ctx) {
+                  return AlertDialog(
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Text("Enter the message to sign:"),
+                        TextField(
+                          controller: controller,
+                          decoration: const InputDecoration(
+                            labelText: 'Message to sign',
+                          ),
+                        ),
+                      ],
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () async {
+                          Future<Signature> sigFuture = KeyManager.instance.sign("solana".codeUnits);
+                          Navigator.pop(ctx);
+                          showDialog(
+                            context: context,
+                            builder: (ctx) {
+                              return AlertDialog(
+                                title: const Text('Signature'),
+                                content: FutureBuilder<Signature>(
+                                  future: sigFuture,
+                                  builder: (ctx, snapshot) {
+                                    if (snapshot.hasData) {
+                                      return Text(
+                                        "Base58: ${base58encode(snapshot.data!.bytes)}\n"
+                                        "Hex: ${snapshot.data!.bytes.map((e) => e.toRadixString(16).padLeft(2, '0')).join()}",
+                                      );
+                                    } else {
+                                      return const Text('Signing...');
+                                    }
+                                  },
+                                ),
+                                actions: [
+                                  TextButton(
+                                    child: const Text('OK'),
+                                    onPressed: () {
+                                      Navigator.of(ctx).pop();
+                                    },
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                        child: const Text("OK"),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            icon: const Icon(CupertinoIcons.signature),
+          ),
+        ],
       ),
       drawer: Drawer(
         child: ListView(
