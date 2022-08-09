f66856c9fac5b85127084825f660034f3c24c2b0
FixedLocally <9797160+FixedLocally@users.noreply.github.com>
2022-08-09 22:43:54 +0800

    Moved functions into menu

--- lib/routes/home.dart
+++ lib/routes/home.dart
@@ -22,68 +22,86 @@ class _HomeRouteState extends State<HomeRoute> {
       appBar: AppBar(
         title: Text(KeyManager.instance.walletName),
         actions: [
-          IconButton(
-            onPressed: () async {
-              TextEditingController controller = TextEditingController();
-              await showDialog<String>(
-                context: context,
-                builder: (ctx) {
-                  return AlertDialog(
-                    title: const Text("Enter the message to sign:"),
-                    content: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        TextField(
-                          controller: controller,
-                          decoration: const InputDecoration(
-                            labelText: 'Message to sign',
-                          ),
+          PopupMenuButton(
+            itemBuilder: (context) {
+              return [
+                const PopupMenuItem(
+                  value: 'sign',
+                  child: Text('Sign Message'),
+                ),
+                const PopupMenuItem(
+                  value: 'create',
+                  child: Text('Create Wallet'),
+                ),
+              ];
+            },
+            onSelected: (s) async {
+              switch (s) {
+                case 'sign':
+                  TextEditingController controller = TextEditingController();
+                  await showDialog<String>(
+                    context: context,
+                    builder: (ctx) {
+                      return AlertDialog(
+                        title: const Text("Enter the message to sign:"),
+                        content: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            TextField(
+                              controller: controller,
+                              decoration: const InputDecoration(
+                                labelText: 'Message to sign',
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                    actions: [
-                      TextButton(
-                        onPressed: () async {
-                          Future<Signature> sigFuture = KeyManager.instance.sign("solana".codeUnits);
-                          Navigator.pop(ctx);
-                          showDialog(
-                            context: context,
-                            builder: (ctx) {
-                              return AlertDialog(
-                                title: const Text('Signature'),
-                                content: FutureBuilder<Signature>(
-                                  future: sigFuture,
-                                  builder: (ctx, snapshot) {
-                                    if (snapshot.hasData) {
-                                      return Text(
-                                        "Base58: ${base58encode(snapshot.data!.bytes)}\n\n"
-                                        "Hex: ${snapshot.data!.bytes.map((e) => e.toRadixString(16).padLeft(2, '0')).join()}",
-                                      );
-                                    } else {
-                                      return const Text('Signing...');
-                                    }
-                                  },
-                                ),
-                                actions: [
-                                  TextButton(
-                                    child: const Text('OK'),
-                                    onPressed: () {
-                                      Navigator.of(ctx).pop();
-                                    },
-                                  ),
-                                ],
+                        actions: [
+                          TextButton(
+                            onPressed: () async {
+                              Future<Signature> sigFuture = KeyManager.instance.sign("solana".codeUnits);
+                              Navigator.pop(ctx);
+                              showDialog(
+                                context: context,
+                                builder: (ctx) {
+                                  return AlertDialog(
+                                    title: const Text('Signature'),
+                                    content: FutureBuilder<Signature>(
+                                      future: sigFuture,
+                                      builder: (ctx, snapshot) {
+                                        if (snapshot.hasData) {
+                                          return Text(
+                                            "Base58: ${base58encode(snapshot.data!.bytes)}\n\n"
+                                                "Hex: ${snapshot.data!.bytes.map((e) => e.toRadixString(16).padLeft(2, '0')).join()}",
+                                          );
+                                        } else {
+                                          return const Text('Signing...');
+                                        }
+                                      },
+                                    ),
+                                    actions: [
+                                      TextButton(
+                                        child: const Text('OK'),
+                                        onPressed: () {
+                                          Navigator.of(ctx).pop();
+                                        },
+                                      ),
+                                    ],
+                                  );
+                                },
                               );
                             },
-                          );
-                        },
-                        child: const Text("OK"),
-                      ),
-                    ],
+                            child: const Text("OK"),
+                          ),
+                        ],
+                      );
+                    },
                   );
-                },
-              );
+                  break;
+                case 'create':
+                  await Utils.showLoadingDialog(context, KeyManager.instance.createWallet());
+                  setState(() {});
+              }
             },
-            icon: const Icon(CupertinoIcons.signature),
           ),
         ],
       ),
@@ -103,17 +121,6 @@ class _HomeRouteState extends State<HomeRoute> {
                           top: 0,
                           left: 0,
                           child: Text('Wallet'),
-                        ),
-                        Positioned(
-                          top: 0,
-                          right: 0,
-                          child: IconButton(
-                            onPressed: () async {
-                              await Utils.showLoadingDialog(context, KeyManager.instance.createWallet());
-                              setState(() {});
-                            },
-                            icon: const Icon(Icons.add),
-                          ),
                         ),
                       ],
                     ),
