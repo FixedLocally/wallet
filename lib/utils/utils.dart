dea4e07e916b09174a389d0c0095a556219e0a67
UtopiaMinT <9797160+UtopiaMinT@users.noreply.github.com>
2022-09-21 01:42:50 +0800

    feat: Action bottom sheet

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -557,6 +557,41 @@ class Utils {
     return result ?? false;
   }
 
+  static Future<int> showActionBottomSheet({
+    required BuildContext context,
+    required String title,
+    required List<BottomSheetAction> actions,
+  }) async {
+    int? result = await showModalBottomSheet<int>(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+      ),
+      builder: (ctx) {
+        return SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: Text(title, style: Theme.of(context).textTheme.headline6),
+              ),
+              ...actions.map((action) => ListTile(
+                title: Text(action.title),
+                leading: action.leading,
+                dense: true,
+                onTap: () {
+                  Navigator.pop(ctx, action.value);
+                },
+              )),
+            ],
+          ),
+        );
+      },
+    );
+    return result ?? -1;
+  }
+
   static Future<bool> showInfoDialog({
     required BuildContext context,
     String? title,
@@ -713,4 +748,16 @@ class SplTokenAccountDataInfoWithUsd extends SplTokenAccountDataInfo {
 
   @override
   int get hashCode => account.hashCode;
+}
+
+class BottomSheetAction {
+  final String title;
+  final Widget? leading;
+  final int value;
+
+  BottomSheetAction({
+    required this.title,
+    this.leading,
+    required this.value,
+  });
 }
