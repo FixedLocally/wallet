aab97eb0c84526e1e879fd7c3f1ef3f52b1187f3
UtopiaMinT <9797160+UtopiaMinT@users.noreply.github.com>
2022-10-04 13:10:17 +0800

    feat: refactored domain info out

---
+++ lib/widgets/domain_info.dart
@@ -0,0 +1,46 @@
+
+import 'package:flutter/material.dart';
+
+import 'image.dart';
+
+class DomainInfoWidget extends StatelessWidget {
+  final String domain;
+  final String title;
+  final List<String> logoUrls;
+
+  const DomainInfoWidget({
+    Key? key,
+    required this.domain,
+    required this.logoUrls,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: theme.colorScheme.surface,
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          padding: EdgeInsets.all(16.0),
+          child: Logo(
+            domain: domain,
+            urls: logoUrls,
+            width: 64,
+            height: 64,
+          ),
+        ),
+        SizedBox(height: 16),
+        Text(
+          title,
+          style: theme.textTheme.headline5,
+          textAlign: TextAlign.center,
+        ),
+        Text(domain),
+      ],
+    );
+  }
+}
